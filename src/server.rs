ca84a614758fabf616cc277155edfd34366d1303
Romeo <romeoago004@gmail.com>
2026-01-13 18:12:42 +0100

    test

---
+++ src/server.rs
@@ -0,0 +1,19 @@
+use axum::{routing::get, Router};
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+mod path;
+
+const ADDRESS: &str = "127.0.0.1:3000";
+
+pub async fn server() {
+    let router = Router::new()
+        .route("/", get(path::home_page))
+        .route("/convert", get(path::convert))
+        .route("/result", get(path::result))
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", path::ApiDoc::openapi()));
+
+    let listener = tokio::net::TcpListener::bind(ADDRESS).await.unwrap();
+
+    println!("Serveur : http://{}", ADDRESS);
+    axum::serve(listener, router).await.unwrap();
+}
